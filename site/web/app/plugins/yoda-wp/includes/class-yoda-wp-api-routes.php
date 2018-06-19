a68f5b4f482a81fbc50700847e1931476a156c60
Brian M. Herold <brian.herold@genesys.com>
2018-06-19 16:59:12 -0500

    hacky cors

--- site/web/app/plugins/yoda-wp/includes/class-yoda-wp-api-routes.php
+++ site/web/app/plugins/yoda-wp/includes/class-yoda-wp-api-routes.php
@@ -50,13 +50,35 @@ class Yoda_WP_API_Routes {
 	 * @since    1.0.0
 	 */
 	public function rest_api_init () {
-		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
-		add_filter( 'rest_pre_serve_request', function( $value ) {
-			header( 'Access-Control-Allow-Origin: *' );
-			header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
-			header( 'Access-Control-Allow-Credentials: true' );
-			return $value;
-		});
+		// remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+		// add_filter( 'rest_pre_serve_request', function( $value ) {
+		// 	header( 'Access-Control-Allow-Origin: *' );
+		// 	header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		// 	header( 'Access-Control-Allow-Credentials: true' );
+		// 	return $value;
+		// });
+
+		// remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+		// add_filter( 'rest_pre_serve_request', function( $value ) {
+		// 	$origin = get_http_origin();
+		// 	if ( $origin && in_array( $origin, array(
+		// 			'localhost:4400'
+		// 		) ) ) {
+		// 		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+		// 		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		// 		header( 'Access-Control-Allow-Credentials: true' );
+		// 	}
+		// 	return $value;
+		// });
+
+		// add_filter( 'allowed_http_origin', '__return_true' );
+
+		// add_filter('http_origin', function() { return "http://localhost:4400";});
+
+		$origin = 'https://localhost:4400';
+		header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
 
 		register_rest_route('api/v1', '/test/(?P<value>.*)', array(
 			'methods' => 'GET',
