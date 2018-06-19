a178fc8d0881d2955055088af3e070bcf923661c
Serge Margovsky <Serge.Margovsky@inin.com>
2018-06-19 10:55:07 -0500

    saving announcements works

--- site/web/app/plugins/yoda-wp/includes/class-yoda-wp.php
+++ site/web/app/plugins/yoda-wp/includes/class-yoda-wp.php
@@ -122,8 +122,13 @@ class Yoda_WP {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-yoda-wp-public.php';
 
-		$this->loader = new Yoda_WP_Loader();
+        /**
+         * The class responsible for sanitizing user input
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-yoda-wp-sanitizer.php';
 
+		$this->loader = new Yoda_WP_Loader();
+        $this->sanitizer = new Yoda_WP_Sanitize();
 	}
 
 	/**
