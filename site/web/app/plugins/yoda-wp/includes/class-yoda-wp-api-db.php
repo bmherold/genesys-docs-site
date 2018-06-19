129d19a8596a3a40556b2d2e1d8c99e7574e7b88
Brian M. Herold <brian.herold@genesys.com>
2018-06-19 13:58:45 -0500

    ability to return all raw posts

--- site/web/app/plugins/yoda-wp/includes/class-yoda-wp-api-db.php
+++ site/web/app/plugins/yoda-wp/includes/class-yoda-wp-api-db.php
@@ -25,7 +25,7 @@ class Yoda_WP_API_DB {
 	}
 
 	public function get_posts() {
-		return $this->queryPosts();
+		return $this->queryPosts(['post_type' => ['wizard', 'post', 'page', 'announcement']], true);
 	}
 
 	public function get_guides($route, $permissions, $users, $use_dummy_data = false) {
@@ -38,7 +38,6 @@ class Yoda_WP_API_DB {
 
 		$wizards = $this->getWizards();
 		// return $wizards;
-
 
 		return array_merge($announcements, $wizards);
 	}
@@ -105,39 +104,19 @@ class Yoda_WP_API_DB {
 	}
 
 	private function queryPosts($options = [], $with_meta = false) {
-		$defaults = [
-			'numberposts' => 5,
-			'category' => 0,
-			'orderby' => 'date',
-			'order' => 'DESC',
-			'include' => array(),
-			'exclude' => array(),
-			'meta_key' => '',
-			'meta_value' =>'',
-			'post_type' => 'post',
-			'suppress_filters' => true
-		];
-		$defaults = array_merge($defaults, $options);
-
-		$r = wp_parse_args( [], $defaults );
-		if ( empty( $r['post_status'] ) )
-				$r['post_status'] = ( 'attachment' == $r['post_type'] ) ? 'inherit' : 'publish';
-		if ( ! empty($r['numberposts']) && empty($r['posts_per_page']) )
-				$r['posts_per_page'] = $r['numberposts'];
-		if ( ! empty($r['category']) )
-				$r['cat'] = $r['category'];
-		if ( ! empty($r['include']) ) {
-				$incposts = wp_parse_id_list( $r['include'] );
-				$r['posts_per_page'] = count($incposts);  // only the number of posts included
-				$r['post__in'] = $incposts;
-		} elseif ( ! empty($r['exclude']) )
-				$r['post__not_in'] = wp_parse_id_list( $r['exclude'] );
-
-		$r['ignore_sticky_posts'] = true;
-		$r['no_found_rows'] = true;
-
-		$get_posts = new WP_Query;
-		$post_results = $get_posts->query($r);
+    // $defaults = array(
+		// 	'numberposts' => 5,
+		// 	'category' => 0,
+		//  'orderby' => 'date',
+		// 	'order' => 'DESC',
+		//  'include' => array(),
+		// 	'exclude' => array(),
+		//  'meta_key' => '',
+		// 	'meta_value' =>'',
+		//  'post_type' => 'post',
+		// 	'suppress_filters' => true
+		// );
+		$post_results = get_posts($options);
 
 		if ($with_meta) {
 			foreach ($post_results as &$post) {
